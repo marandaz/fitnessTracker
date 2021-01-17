be8367e6b67185540fd9a07f4bc1ae9bfeb5cd1b
marandaz <mrzwies@gmail.com>
2021-01-17 13:11:16 -0600

    changed api route file name to separate from api.js in public directory

---
+++ routes/api-routes.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workout", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workout/bulk", ({ body }, res) => {
+  Workout.insertMany(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  const workoutId = req.params.id;
+  Workout.findOneAndUpdate({ _id: workoutId }, { $push: { exercises: req.body } }, { new: true })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: ["$exercises.duration"]
+        }
+      }
+    }
+  ]
+  )
+    .sort({ day: "desc" })
+    .limit(7)
+    .sort({ day: "asc" })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
