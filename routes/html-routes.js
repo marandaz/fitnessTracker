6f63c4eab1675fe8be7e87454a1c388cd8864c4d
marandaz <mrzwies@gmail.com>
2021-01-17 12:56:50 -0600

    added html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+module.exports = function (app) {
+    // Index Route
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    // Stats Route
+    app.get("/stats", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+    // Exercise Route
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+};
